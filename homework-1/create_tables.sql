aac008e5f3f32e9190e3a3a5dc784c835411712b
AlisaV365 <127328313+AlisaV365@users.noreply.github.com>
2023-05-25 21:59:00 +0300

    HomeWork - 1 - Создана БД `north` - Созданы 3 таблицы:     - `employees`     - `customers`     - `orders` - Sql-код для создания таблиц скопирован в файл `create_tables.sql`

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,29 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers
+(
+	id serial PRIMARY KEY,
+	customer_id varchar(50) NOT NULL,
+	company_name varchar(50) NOT NULL,
+	contact_name varchar(50) NOT NULL
+);
+
+
+CREATE TABLE employees
+(
+	first_name varchar(50),
+	last_name varchar(50),
+	title varchar(100),
+	birth_date date,
+	notes text
+);
+
+CREATE TABLE orders
+(
+	id serial PRIMARY KEY,
+	order_id int NOT NULL,
+	customer_id varchar(50) NOT NULL,
+	employee_id int NOT NULL,
+	order_date varchar(50) NOT NULL,
+	ship_city varchar(50) NOT NULL
+);
+
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,55 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import psycopg2
+import csv
+
+
+def read_csv():
+    data = []
+    with open('north_data/customers_data.csv', 'r', encoding='utf-8') as file:
+        file_reader = csv.reader(file, delimiter=',')
+        for line in file_reader:
+            data.append(line)
+
+    return data
+
+
+def read_csv_employees():
+    data_employees = []
+    with open('north_data/employees_data.csv', 'r', encoding='utf-8') as file:
+        file_reader = csv.reader(file, delimiter=',')
+        for line in file_reader:
+            data_employees.append(line)
+    return data_employees
+
+
+def read_csv_orders():
+    data_orders = []
+    with open('north_data/orders_data.csv', 'r', encoding='utf-8') as file:
+        file_reader = csv.reader(file, delimiter=',')
+        for line in file_reader:
+            data_orders.append(line)
+    return data_orders
+
+
+def main():
+    conn = psycopg2.connect(host='localhost', database='north', user='alisavorotnikova', password='cxz3t55')
+    data = read_csv()
+    data_employees = read_csv_employees()
+    data_orders = read_csv_orders()
+    with conn:
+        cursor = conn.cursor()
+        for item in data[1:]:
+            cursor.execute(
+                f"INSERT INTO customers (customer_id, company_name, contact_name) VALUES ('{item[0]}', '{item[1]}', '{item[2]}')")
+
+        for item in data_employees[1:]:
+            cursor.execute(
+                f"INSERT INTO employees (first_name, last_name, title, birth_date, notes) VALUES ('{item[0]}', '{item[1]}', '{item[2]}', '{item[3]}', '{item[4]}')")
+
+        for item in data_orders[1:]:
+            cursor.execute(
+                f"INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES ({int(item[0])}, '{item[1]}', {int(item[2])}, '{item[3]}', '{item[4]}')")
+
+
+if __name__ == '__main__':
+    main()
